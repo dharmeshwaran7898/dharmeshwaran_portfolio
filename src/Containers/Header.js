218961c61e78f57c6f305e5210c18bf5721368da
aditya sawant <aditya.sawant@csinfocomm.com>
2023-12-15 15:47:19 +0530

    first commit

---
+++ src/Containers/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="dwn_Header">
+      <div className="left_section">
+        <h1 className="mySelf">
+          Hi! <br /> MySelf <br />
+          xyz xyz
+        </h1>
+        <p className="dwn_aboutMe">
+          i am front-end developer with 2+ years of experience who is
+          comfortable working with next js , react js ,Javascript, HTML/CSS, and
+          responsive web design to deliver exceptional customer experience. I
+          have proven experience in creating and designing software in a
+          test-driven environment
+        </p>
+      </div>
+      <div className="right_section">
+        <img src="" alt="myImage" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
